15ce9b360cf6ae85e0a5665b035f411dccc5d2e9
virat <viratt737@gmail.com>
2026-04-24 17:39:29 +0530

    create ui first in client side we create 3 routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCreadit from './pages/BuyCreadit'
+import Result from './pages/Result'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buycreadits' element={<BuyCreadit/>}/>
+        
+      </Routes>
+      
+    </div>
   )
 }
 
---
+++ Client/src/pages/BuyCreadit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuyCreadit = () => {
+  return (
+    <div>BuyCreadit</div>
+  )
+}
+
+export default BuyCreadit
