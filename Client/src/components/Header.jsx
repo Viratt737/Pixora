00f7388db2df49ff7ad6089046032f7a7df8ae03
virat <viratt737@gmail.com>
2026-04-30 00:55:48 +0530

    add some animation on web page

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {assets} from '../assets/assets'
+import { motion } from "motion/react"
+import {AppContext} from '../context/AppContext'
+import {useNavigate} from 'react-router-dom'
 const Header = () => {
+    const {user, setShowLogin} = useContext(AppContext)
+    const navigate = useNavigate()
+    const onClickHandler =() =>{
+        if(user){
+            navigate('/result')
+        }else{
+            setShowLogin(true)
+        }
+    }
   return (
-    <div className='flex flex-col justify-center items-center
-     text-center my-20'>
+    <motion.div className='flex flex-col justify-center items-center
+     text-center my-20'
+     initial={{opacity:0.2, y:100}}
+     transition={{duration:1}}
+     whileInView={{opacity:1, y:0}}
+     viewport={{once:true}}>
         <div className='text-stone-500 inline-flex text-center gap-2
         bg-white px-6 py-1 rounded-full border border-neutral-500'>
             <p>Best text to image generator</p>
@@ -15,8 +31,9 @@ const Header = () => {
          imagination into visual art in seconds - just type, and
          watch the magic happen.
         </p>
-        <button className='text-white text-xl bg-black
-        w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full hover:scale-105'>Generate Images
+        <button onClick={onClickHandler}
+        className='text-white text-xl bg-black
+        w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full hover:scale-105 cursor-pointer'>Generate Images
             <img className="h-6" src={assets.star_group} alt="" />
         </button>
 
@@ -29,7 +46,7 @@ const Header = () => {
         </div>
 
         <p className='mt-2 text-neutral-600'>Generated images from Pixora</p>
-    </div>
+    </motion.div>
   )
 }
 
