a6fbc69f3c03451333cba363467f38ae16ec829f
virat <viratt737@gmail.com>
2026-05-01 20:10:16 +0530

    now generate image api is complete

---
+++ Server/routes/image.route.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import {generateImage} from '../controllers/image.controller.js'
+import userAuth from '../middleware/auth.middleware.js'
+const imageRouter = express.Router()
+imageRouter.post('/generate-image',userAuth ,generateImage)
+
+export default imageRouter
--- Server/server.js
+++ Server/server.js
@@ -3,12 +3,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectTODb from './config/db.js'
 import userRouter from './routes/user.route.js'
+import imageRouter from './routes/image.route.js'
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json())
 app.use(cors())
 connectTODb()
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 app.get('/' ,(req, res) =>{
     res.json({
         "msg" : "API is Working"
