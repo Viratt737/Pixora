a571949843f3b832e53b6c6723018acebcb71e36
virat <viratt737@gmail.com>
2026-05-05 09:53:05 +0530

    all done in website but payment gatway not add till now.

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react'
 export const AppContext = createContext()
 import {toast} from 'react-toastify'
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 const AppContextProvider = (props) =>{
     const [user, setUser] = useState(null)
     const [showLogin, setShowLogin] = useState(false);
@@ -10,6 +11,7 @@ const AppContextProvider = (props) =>{
     const [credit, setCredit] = useState(false)
     const backendurl = import.meta.env.VITE_BACKEND_URL
     
+    const navigate = useNavigate()
     const loadCreditsData = async () =>{
         try{
             const {data} = await axios.get(backendurl + '/api/user/credits', {headers: {token}})
@@ -22,7 +24,25 @@ const AppContextProvider = (props) =>{
             toast.error(error.message)
         }
     }
-     
+    const generateImage = async (prompt) =>{
+        try{
+          const {data} = await axios.post(backendurl + '/api/image/generate-image', {prompt} , {headers : {token}})
+           if(data.success){
+               loadCreditsData()
+               return data.resultImage
+            }else{
+                toast.error(data.message)
+                loadCreditsData()
+                if(data.createBalance === 0){
+                   navigate('/buycredits')
+                }
+            }
+        }catch(error){
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+ 
     const logout = () =>{
         localStorage.removeItem('token');
         setToken('')
@@ -35,7 +55,8 @@ const AppContextProvider = (props) =>{
     }, [token])
     const value = {
         user, setUser, showLogin, setShowLogin, backendurl,
-        token, setToken, credit, setCredit, loadCreditsData, logout
+        token, setToken, credit, setCredit, loadCreditsData, logout,
+        generateImage
     }
     return (
         <AppContext.Provider value={value}>
--- Client/src/pages/Result.jsx
+++ Client/src/pages/Result.jsx
@@ -1,14 +1,26 @@
-import React,{useState} from 'react'
+import React,{useState, useContext} from 'react'
 import {assets} from '../assets/assets'
 import { motion } from "motion/react"
+import {AppContext} from '../context/AppContext'
 const Result = () => {
   
    const [image, setImage] = useState(assets.sample_img_1)
    const [isImageLoaded, setIsImageLoaded] = useState(false)
    const [loading, setLoading] = useState(false)
    const [input, setInput] = useState('')
+   const {generateImage} = useContext(AppContext)
    const onSubmitHandler = async (e) =>{
+       e.preventDefault()
+       setLoading(true)
 
+       if(input){
+         const image = await generateImage(input)
+         if(image){
+          setIsImageLoaded(true)
+          setImage(image)
+         }
+       }
+       setLoading(false)
    }
   return (
     <motion.form 
